488451b0e1bd59effcc86b1c8877bf94b687ef9d
codeslayer <willyanpublic@protonmail.com>
2023-03-26 15:47:21 -0300

    replace the benchmark module by criterion

---
+++ benches/utgraph_bench.rs
@@ -0,0 +1,28 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use algraphs::graphs::utgraph::UTGraph;
+use algraphs::models::*;
+
+const MODEL: &GraphData = &GRAPHS[5];
+
+fn get_deg_bench(c: &mut Criterion) {
+    let graph = UTGraph::new(MODEL.verts).edges(MODEL.edges);
+    c.bench_function("get degree", |b| b.iter(|| graph.degree(black_box(6))));
+}
+
+fn max_deg_bench(c: &mut Criterion) {
+    let graph = UTGraph::new(MODEL.verts).edges(MODEL.edges);
+    c.bench_function("max degree", |b| b.iter(|| graph.max_deg()));
+}
+
+fn is_star_bench(c: &mut Criterion) {
+    let graph = UTGraph::new(MODEL.verts).edges(MODEL.edges);
+    c.bench_function("is star", |b| b.iter(|| graph.is_star()));
+}
+
+fn path_bench(c: &mut Criterion) {
+    let graph = UTGraph::new(MODEL.verts).edges(MODEL.edges);
+    c.bench_function("find path", |b| b.iter(|| graph.path(black_box(0), black_box(6))));
+}
+
+criterion_group!(benches, get_deg_bench, max_deg_bench, is_star_bench, path_bench);
+criterion_main!(benches);
--- src/main.rs
+++
@@ -1,7 +0,0 @@
-use algraphs::benchmark;
-
-fn main() {
-    let joke = "My software never has bugs. It just develops random features.";
-    let perf_msg = benchmark::med_exec_time(&|| joke).msg("Joke");
-    println!("{perf_msg}");
-}
